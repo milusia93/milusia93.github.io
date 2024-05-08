981156c1f29e1c902af404ff515de0e04a5020e7
MilenaD <44272975+milusia93@users.noreply.github.com>
2024-05-08 22:48:03 +0200

    skonczone

---
+++ zadania-domowe/5-menu-boczne-i-efekt-wyjscia/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    $('.hamburger').click(function() {
+
+        $('.main-menu').toggleClass('open');
+
+    });
+
+    $('.main-menu-item a').click(function(e) {
+        e.preventDefault();
+        $('body').fadeOut(2000, function(){
+            window.location.href = $(e.target).attr('href');
+        });
+
+    });
+
+    // $('.hamburger').click(function() {
+
+    //     $('.main-menu').toggleClass('open');
+
+    // });
+
+    // $('.main-menu-item a').click(function(e) {
+    //     let item = $(this);
+    //     console.log(e);
+    //     e.preventDefault();
+    //     $('body').fadeOut(2000, function(){
+    //         window.location.href = item.attr('href');
+    //     });
+
+    // });
+
+});
